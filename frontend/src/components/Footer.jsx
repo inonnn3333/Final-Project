0f5b38c20e06024a98463c94b7bcb4f469a2b527
Inon_Moshe_Yosef <inonnn333@gmail.com>
2024-11-17 01:03:19 +0200

    css footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,11 +7,30 @@ const Footer = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='btnDiv'>
-            <button className='button' onClick={() => navigate('/')}>כל השיעורים</button>
-            <button className='button' onClick={() => navigate('/my-favorites')}>שיעורים שהזמנתי</button>
-            <button className='button' onClick={() => navigate('/my-users')}>הלקוחות שלי</button>
-            <button className='button'>משתתפים בשיעורים</button>
+        <div className='footer-container'>
+            <div className='btnDiv'>
+                <button
+                    className='button'
+                    onClick={() => navigate('/')}>
+                        <img src="/images/home-icon.png" alt="profile-icon" />
+                        <p>בית</p>
+                </button>
+                <button
+                    className='button'
+                    onClick={() => navigate('/my-favorites')}>
+                        <img src="/images/heart-icon.png" alt="profile-icon" />
+                        <p>בחירותיי</p>
+                </button>
+                <button
+                    className='button'
+                    onClick={() => navigate('/')}>
+                        <img src="/images/profile-icon.png" alt="profile-icon" />
+                        <p>פרופיל</p>
+                </button>
+
+                {/* <button className='button' onClick={() => navigate('/my-users')}>הלקוחות שלי</button>
+                <button className='button'>משתתפים בשיעורים</button> */}
+            </div>
         </div>
     )
 }
